a8e972b8311f1fffddab9137819b12ed54fefc20
Adrine UWERA <a.uwera@alustudent.com>
2022-03-20 17:01:12 +0200

    modify and add hero pages

---
+++ heroes_website/authentication/index.php
@@ -0,0 +1,12 @@
+<?php 
+session_start();
+if (!isset($_SESSION['Logged_user'])) {
+	header('Location:login.php');
+}	
+?>
+welcome
+
+<form method="post">
+	<?php require_once('operations.php') ?>
+	<button type="submit" name="logout" class="btn btn-danger">Logout</button>
+</form>
