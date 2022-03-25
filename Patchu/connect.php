5fac9fabec2e0dd83b58e4c117dd71e74a83c853
Adrine UWERA <a.uwera@alustudent.com>
2022-03-25 16:30:24 +0200

    organized the files

--- Patchu/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $con = new mysqli('localhost', 'root', '','crude_operation');
-    if($con){
-        echo "connection successfully";
-    }
-    else{
-        die(mysqli_error($con)); 
-    }
-?>
--- Patchu/log_out.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    session_unset();
-    session_destroy();
-    header('location:authentication.php');
-?>
-<h1?>welcome you are authenticated...</h1>
-<a href="log_out">log ..out!</a>
